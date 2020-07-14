2092dda0e0a82142f3343e3febdf471e76b154ce
s <you@example.com>
2020-07-15 01:13:27 +0200

    after category

--- dalil/admin.py
+++ dalil/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import numbers
+from .models import numbers, cate
 
 
 admin.site.register(numbers)
+admin.site.register(cate)
--- dalil/models.py
+++ dalil/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+
+class cate(models.Model):
+    catname = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.catname
+
+
 type_n = (
     ('pub', 'public'),
     ('pri', 'private'),
@@ -12,6 +20,7 @@ class numbers(models.Model):
     phone_n = models.CharField(max_length=15)
     name = models.CharField(max_length=30)
     n_type = models.CharField(max_length=10, choices=type_n)
+    cat = models.ForeignKey(cate, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
