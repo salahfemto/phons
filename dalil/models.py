c7f22bb4cd2d1ca4f2be8f8dd273922c23dc6319
s <you@example.com>
2020-07-15 00:48:21 +0200

    after data

--- dalil/models.py
+++ dalil/models.py
@@ -2,7 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+type_n = (
+    ('pub', 'public'),
+    ('pri', 'private'),
+)
+
 
 class numbers(models.Model):
     phone_n = models.CharField(max_length=15)
     name = models.CharField(max_length=30)
+    n_type = models.CharField(max_length=10, choices=type_n)
+
+    def __str__(self):
+        return self.name
