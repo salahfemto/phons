76cb16917e0011046b06b47712fcc0cf428c5350
s <you@example.com>
2020-07-14 20:46:18 +0200

    after create models

--- dalil/models.py
+++ dalil/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class numbers(models.Model):
+    phone_n = models.CharField(max_length=15)
+    name = models.CharField(max_length=30)
